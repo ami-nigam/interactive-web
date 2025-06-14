5faafbe032aa21cdff22832757f5739107d64a7e
ami-nigam <ami.nigam92@gmail.com>
2025-06-14 13:03:36 +0100

    Fix vite config with correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-@"
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/interactive-web/'
 })
-"@ | Out-File -FilePath "vite.config.js" -Encoding UTF8
